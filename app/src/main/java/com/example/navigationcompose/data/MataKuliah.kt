e92a6660ab04d85ee9a8dcb3fc1cf59070abf718
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-11-21 09:50:34 +0700

    Membuat object MataKuliah dengan val list

--- app/src/main/java/com/example/navigationcompose/data/MataKuliah.kt
+++ app/src/main/java/com/example/navigationcompose/data/MataKuliah.kt
@@ -1,5 +1,12 @@
 package com.example.navigationcompose.data
 
 object MataKuliah {
-
+    val option = listOf(
+        "Pemrogaram Aplikasi Mobile Lanjut",
+        "Pemrogaram Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi MultiMedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
 }
