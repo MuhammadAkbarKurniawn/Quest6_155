405a78dbac793c24e05014eba1742d28aa6ab5eb
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-11-21 10:05:32 +0700

    Membuat variabel variabel pada RencanaStudiView

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudiView.kt
@@ -1,6 +1,10 @@
 package com.example.navigationcompose.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.navigationcompose.model.Mahasiswa
 
 @Composable
@@ -8,4 +12,10 @@ fun RencanaStudiView(
     mahasiswa: Mahasiswa,
     OnSubmitButtonClicked: (MutableList<String>) -> Unit,
     OnBackButtinClicked: () ->Unit,
-){}
+){
+    var chosenDropdown by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember { mutableStateOf("") }
+
+    var listData: MutableList<String> = mutableListOf(chosenDropdown,pilihanKelas)
+}
