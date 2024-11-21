565b3bee2da5996e1a4ba45d62e7900ba8c67285
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-11-21 10:39:26 +0700

    Menambahkan ExposedDropdownMenuBox pada function DynamicSelectedField

--- app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
@@ -1,6 +1,12 @@
 package com.example.navigationcompose.ui.widget
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,6 +15,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DynamicSelectedField(
     selectedvalue : String,
@@ -19,5 +26,23 @@ fun DynamicSelectedField(
 ){
     var expanded by remember { mutableStateOf(false) }
 
-
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedvalue,
+            onValueChange = {},
+            label = { Text(text = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+    }
 }
