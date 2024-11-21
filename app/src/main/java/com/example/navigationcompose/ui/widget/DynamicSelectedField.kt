90c8fb0a2eea73e4bd68a63200f097451622930b
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-11-21 10:33:32 +0700

    Membuat variabel expanded dengan nilai false

--- app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
@@ -1,6 +1,11 @@
 package com.example.navigationcompose.ui.widget
 
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 
@@ -12,5 +17,7 @@ fun DynamicSelectedField(
     onValueChangeEvent: (String) -> Unit,
     modifier: Modifier = Modifier
 ){
+    var expanded by remember { mutableStateOf(false) }
+
 
 }
