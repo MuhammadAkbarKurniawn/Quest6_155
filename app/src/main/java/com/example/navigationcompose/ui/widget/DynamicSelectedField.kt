1982199f598fb38c9ae438b510b841637b190260
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-11-21 10:30:25 +0700

    Sebelumnya kita membuat DynamicSelectedField/Dropdown untuk melanjutkn membuat view rencanastudi

---
+++ app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
@@ -0,0 +1,16 @@
+package com.example.navigationcompose.ui.widget
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
+
+@Composable
+fun DynamicSelectedField(
+    selectedvalue : String,
+    option: List<String>,
+    label : String,
+    onValueChangeEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
