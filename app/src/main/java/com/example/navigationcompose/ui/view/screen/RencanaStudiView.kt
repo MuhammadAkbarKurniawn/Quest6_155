eaa484ac86a04ca5c5e9a5503175d15b039f71b9
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-11-21 10:08:19 +0700

    Membuat Column dengan ukuran penuh dan warna yang kita ambil dari resource

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudiView.kt
@@ -1,10 +1,16 @@
 package com.example.navigationcompose.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.navigationcompose.R
 import com.example.navigationcompose.model.Mahasiswa
 
 @Composable
@@ -18,4 +24,12 @@ fun RencanaStudiView(
     var pilihanKelas by remember { mutableStateOf("") }
 
     var listData: MutableList<String> = mutableListOf(chosenDropdown,pilihanKelas)
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ){
+
+    }
 }
